5a63bca13c3689bbbc8ebb06059afffba6bfcf65
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-29 16:03:31 -0700

    Minor refactored encode

--- encode.cpp
+++ encode.cpp
@@ -24,20 +24,16 @@ int main(int argc, char* argv[]) {
 	    char ch;
 		std::cin >> std::noskipws >> ch;
 
+		//Prevent taking padding from std cin
 		if (!std::cin.eof())
 			allcode += table[std::to_string(ch)];
 	}
 
+	//Calculate padding offset for the last byte
 	int offset = (allcode.length() % 8 != 0) ? 8 - allcode.length() % 8 : 0;
-
 	for (int i = 0; i < offset; i++)
 		allcode += '0';
-
-	auto offset_identifier = std::bitset<8>(offset);
-	allcode = offset_identifier.to_string() + allcode;
-
-	// std::cout << offset;
-	// std::cout << allcode;
+	allcode = std::bitset<8>(offset).to_string() + allcode;
 
 	// Output binary
 	for (int i = 0; i < allcode.length(); i += 8) {

4840072cb42e3e8f93ab70363c87495c9b2150a6
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-29 15:29:14 -0700

    Fix decode with proper handling of last byte

--- decode.cpp
+++ decode.cpp
@@ -15,36 +15,73 @@ int main(int argc, char* argv[]) {
 	//Get code words from file
 	std::string word, code;
 	std::unordered_map<std::string, std::string> table;
-	while (reader >> word >> code) {
+	while (reader >> word >> code)
 	    table[code] = word;
-	}
+
+	//Get offset identifier
+	char firstByte;
+	std::cin >> std::noskipws >> firstByte;
+	int offset = int(std::bitset<8>(firstByte).to_ulong());
 
 	//Get input from std
-	std::string leftover = "", prevCode = "";
+	std::string leftover = "";
 	while (!std::cin.eof()) {
 
-	    char ch;
-		std::cin >> std::noskipws >> ch;
-		std::string code = std::bitset<8>(ch).to_string();
+		//Initialize codes bytes
+		std::string code1 = "", code2 = "", code3 = "";
+		char byte1, byte2, byte3;
 
-		prevCode = code;
+		//Read first byte
+		std::cin >> std::noskipws >> byte1;
+		code1 = std::bitset<8>(byte1).to_string();
+
+		//Handle first byte
+		if (std::cin.eof())
+			break; //Ignore the last byte since it's duplicated.
+
+		//Read second byte
+		std::cin >> std::noskipws >> byte2;
+		code2 = std::bitset<8>(byte2).to_string();
+
+		//Handle second byte
+		if (std::cin.eof()) {
+			code2 = ""; //Ignore the 2nd byte and keep 1st byte
+		}
+		else {
+
+			//Read third byte
+			std::cin >> std::noskipws >> byte3;
+			code3 = std::bitset<8>(byte3).to_string();
+
+			//Handle third byte
+			if (std::cin.eof())
+				code3 = ""; //Ignore third byte and keep 1st and 2nd bytes.
+		}
+
+		//Combine all bytes
+		code = code1 + code2 + code3;
 		code = leftover + code;
 		leftover = "";
 
+		//Match and print the original words
 		std::string subcode = "";
 		for (int i = 0; i < code.length(); i++) {
-
 			subcode += code[i];
+
+			//Exit if we meet the padding offset
+			if (std::cin.eof() && (code.length() - i) == offset) {
+				break;
+			}
+
+			//Try to match and print codeword
 			if (table.count(subcode) >= 1) {
-
 				char word = stoi(table[subcode]);
-				if (!std::cin.eof() || code != prevCode)
-					std::cout << word;
-
+				std::cout << word;
 				subcode = "";
 			}
 		}
 
+		//Send leftover bytes to next inputs
 		leftover = subcode;
 	}
 
--- encode.cpp
+++ encode.cpp
@@ -24,15 +24,20 @@ int main(int argc, char* argv[]) {
 	    char ch;
 		std::cin >> std::noskipws >> ch;
 
-		if (!std::cin.eof() || ch != '\n')
+		if (!std::cin.eof())
 			allcode += table[std::to_string(ch)];
 	}
 
-	int offset = allcode.length() % 8;
-	std::string padding = (allcode[allcode.length() - 1] == '0') ? "1" : "0";
+	int offset = (allcode.length() % 8 != 0) ? 8 - allcode.length() % 8 : 0;
 
 	for (int i = 0; i < offset; i++)
-		allcode += padding;
+		allcode += '0';
+
+	auto offset_identifier = std::bitset<8>(offset);
+	allcode = offset_identifier.to_string() + allcode;
+
+	// std::cout << offset;
+	// std::cout << allcode;
 
 	// Output binary
 	for (int i = 0; i < allcode.length(); i += 8) {
